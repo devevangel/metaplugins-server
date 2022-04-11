be1924cd4d9043b3aa186aae81f3901e023e9c20
Evangel <dev.evangel@gmail.com>
2022-04-11 03:31:49 +0100

    Rolled back change

--- controllers/pluginController.js
+++ controllers/pluginController.js
@@ -21,7 +21,7 @@ exports.uploadFiles = catchAsync(async (req, res, next) => {
   const uploadedPlugin = await cloudinary.uploader.upload(
     req.files["plugin"][0].path,
     {
-      folder: `metaplugin/Plugins`,
+      folder: `metaplugin/Plugins/plugin-${req.body.name}`,
       public_id: `${req.body.name}`,
       resource_type: "auto",
       overwrite: true,
